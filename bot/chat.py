31beacf6da48055188d1253e3e16beb119eedd37
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-04-06 12:15:09 +0530

    Refactor chatbot architecture: integrate MongoDB for conversation storage, implement chat API, and remove unused files

--- bot/chat.py
+++ bot/chat.py
@@ -3,8 +3,8 @@ import json
 
 import torch
 
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+from bot.model import NeuralNet
+from bot.nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
